f6ba2e71eb233325388cc202bd8b5e2c5486af6f
thorsten zerha <thorsten.zerha@tastenwerk.com>
2014-12-08 23:49:58 +0100

    tests working

--- spec/api/v1/auth_api_spec.rb
+++ spec/api/v1/auth_api_spec.rb
@@ -11,4 +11,9 @@ describe Caminio::Sky::API::Auth do
     expect( last_response.status ).to be == 201
   end
 
+  it "fails with wrong login" do
+    post '/api/v1/auth', login: 'invalid', password: user.password
+    expect( last_response.status ).to be == 401
+  end
+
 end
